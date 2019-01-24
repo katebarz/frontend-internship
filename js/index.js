dd9a2d767d3ddd344a1b463672fccfbece95b350
katebarz <k.barzakovska@gmail.com>
2019-01-24 15:51:35 +0100

    second task

--- js/index.js
+++ js/index.js
@@ -11,3 +11,12 @@ document.getElementById('close').onclick = function() {
     document.getElementById("popup").style.transition = ".5s";
     document.getElementById("show-popup-form").style.display = "block";
 }
+document.getElementById("button").onclick = function() {
+    let massage;
+    document.forms['frm'].login.value === "" ? massage = 'Please enter your login' : massage = '';
+    document.getElementById("validation-login").innerHTML = massage;
+    document.forms['frm'].password.value === "" ? massage = 'Please enter your password' : massage = '';
+    document.getElementById("validation-password").innerHTML = massage;
+    document.getElementById("check-box").checked ? massage = '' : massage = 'Please indicate that you agree to the terms';
+    document.getElementById("validation-check-box").innerHTML = massage;
+}
