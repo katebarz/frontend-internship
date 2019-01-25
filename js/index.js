81022344c421fc925509381b2e342803f1cef5d0
katebarz <k.barzakovska@gmail.com>
2019-01-25 10:13:04 +0100

    third task

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 /* Here goes your JS code */
-document.getElementById('show-popup-form').onclick = function() {
+document.getElementById('show-popup-form').onclick = e => {
     document.getElementById("popup").style.visibility = "visible";
     document.getElementById("popup").style.opacity = "1";
     document.getElementById("popup").style.transition = ".5s";
     document.getElementById("show-popup-form").style.display = "none";
 }
-document.getElementById('close').onclick = function() {
+document.getElementById('close').onclick = e => {
     document.getElementById("popup").style.visibility = "hidden";
     document.getElementById("popup").style.opacity = "0";
     document.getElementById("popup").style.transition = ".5s";
     document.getElementById("show-popup-form").style.display = "block";
 }
-document.getElementById("button").onclick = function() {
+document.getElementById("button").onclick = e => {
     let message1, message2, message3;
     document.forms['frm'].login.value === "" ? message1 = 'Please enter your login' : message1 = '';
     document.getElementById("validation-login").innerHTML = message1;
@@ -19,13 +19,12 @@ document.getElementById("button").onclick = function() {
     document.getElementById("validation-password").innerHTML = message2;
     document.getElementById("check-box").checked ? message3 = '' : message3 = 'Please indicate that you agree to the terms';
     document.getElementById("validation-check-box").innerHTML = message3;
-    //console.log(message1,message2,message2);
-    if(!message1 && !message2 && !message3){
-    	setTimeout( hide, 3000);
+    if (!message1 && !message2 && !message3) {
+        setTimeout(hide, 3000);
     }
-    
 }
-function hide(){
-	document.getElementById("popup").style.visibility = "hidden";
-	document.getElementById("thank-you").innerHTML='Thank you';
+
+function hide() {
+    document.getElementById("popup").style.visibility = "hidden";
+    document.getElementById("thank-you").innerHTML = 'Thank you';
 }
